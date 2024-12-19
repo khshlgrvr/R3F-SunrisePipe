3618f88680db02dc134da180a74c139367c07430
Khushal Grover <90594296+khushalGrover@users.noreply.github.com>
2024-12-19 17:08:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'R3F-Easyjob2',
+ base: process.env.GITHUB_PAGES ? '/R3F-SunrisePipe/' : '/',
   plugins: [react()],
   server: {
     host: true
